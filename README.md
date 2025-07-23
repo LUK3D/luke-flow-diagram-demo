c43e245fe84ed02051908fc28b4a75f16dc57d85
Filipe Lukebana <filipelukebana@gmail.com>
2025-07-23 08:10:46 -0300

    initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# luke_flow_diagram_demo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/luke_flow_diagram_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.luke_flow_diagram_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
